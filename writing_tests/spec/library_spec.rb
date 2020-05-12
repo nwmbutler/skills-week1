d8869b76e97d286de14a633645581d0b91cec73d
Nick Butler <nwmbutler@gmail.com>
2020-05-12 12:00:32 +0100

    first commit

---
+++ writing_tests/spec/library_spec.rb
@@ -0,0 +1,13 @@
+describe Library do
+  it 'can find a specific book' do
+  end
+
+  it 'can add a new book' do
+  end
+
+  it 'can remove a book' do
+  end
+
+  it 'can list all the books on a specific subject' do
+  end
+end
